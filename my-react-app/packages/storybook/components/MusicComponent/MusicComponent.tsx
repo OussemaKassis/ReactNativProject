92a176f4e7b88f60ddebebc68aa532c3776fe933
OussemaKassis <oussemakassis97@gmail.com>
2022-12-06 20:10:39 +0000

    [TASK] create new component skeleton

---
+++ my-react-app/packages/storybook/components/MusicComponent/MusicComponent.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MusicComponent = () => {
+  return (
+    <View>
+      <Text>MusicComponent</Text>
+    </View>
+  )
+}
+
+export default MusicComponent
+
+const styles = StyleSheet.create({})
